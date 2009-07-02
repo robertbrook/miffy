0555b7ec88363905bad2c71c83c23e47b7d9045f
robmckinnon <rob@theyworkforyou.co.nz>
2009-07-02 18:07:47 +0100

    improved handling of overlapping PdfTags and ETags

--- lib/mifparser.rb
+++ lib/mifparser.rb
@@ -85,32 +85,113 @@ class MifParser
     end
   end
 
+  def handle_etag element, xml
+    tag = clean(element)
+    @stack << tag
+    uid = element.at('../Unique/text()').to_s
+    collapsed = element.at('../Collapsed/text()').to_s == 'Yes'
+    if (collapsed || (tag == 'ClauseText')) && @in_paragraph #
+      line = xml.pop
+      lines = []
+      while !line.include?(@pgf_tag)
+        if line[/PgfNumString/]
+          pgf_num_string = line
+        else
+          lines << line
+        end
+        line = xml.pop
+      end
+      pgf_start_tag = line
+      lines.each {|line| add line}
+      add "<#{tag} id='#{uid}'>"
+      add pgf_start_tag
+      puts uid if pgf_num_string.nil?
+      puts tag if pgf_num_string.nil?
+      add pgf_num_string
+      @opened_in_paragraph.clear
+    else
+      add "<#{tag} id='#{uid}'>"
+      @opened_in_paragraph[tag] = true if @in_paragraph
+    end
+  end
+
+  def handle_para xml
+    if @in_paragraph
+      if @opened_in_paragraph.size > 1
+        raise "can not handle all elements opened in <#{@pgf_tag}> paragraph: #{@opened_in_paragraph.keys.inspect}"
+      elsif @opened_in_paragraph.size == 1
+        last_line = xml.pop
+        if last_line.include?(@opened_in_paragraph.keys.first)
+          add "</#{@pgf_tag}>\n"
+          add last_line
+          @pgf_tag = nil
+          @in_paragraph = false
+          @opened_in_paragraph.clear
+        else
+          raise "too tricky to close <#{@pgf_tag}> paragraph, opened element: #{@opened_in_paragraph.keys.first} last_line: #{last_line} xml: #{xml.join("\n").reverse[0..1000].reverse}"
+        end
+      else
+        add "</#{@pgf_tag}>\n"
+        @pgf_tag = nil
+        @in_paragraph = false
+        @opened_in_paragraph.clear
+      end
+    end
+  end
+
+  def handle_element_end element, xml
+    tag = @stack.pop
+    if @in_paragraph && !@opened_in_paragraph[tag]
+      # need to close paragraph
+      add "</#{@pgf_tag}>\n"
+      @pgf_tag = nil
+      @in_paragraph = false
+      @opened_in_paragraph.clear
+    end
+    @opened_in_paragraph.delete(tag)
+    add "</#{tag}>"
+    add "\n" unless tag[/(Day|STHouse|STLords|STText|ClauseTitle|Para|OrderPreamble)/]
+  end
+
+  def add text
+    if text.nil?
+      raise 'text should not be null'
+    else
+      @xml << text
+    end
+  end
+
   def handle_flow flow, xml
-    stack = []
+    @xml = xml
+    @pgf_tag = nil
+    @in_paragraph = false
+    @opened_in_paragraph = {}
+    @stack = []
     flow.traverse_element do |element|
       case element.name
+        when 'PgfTag'
+          @pgf_tag = clean(element).gsub(' ','_')
         when 'ETag'
-          tag = clean(element)
-          stack << tag
-          xml << '<'
-          xml << tag
-          xml << '>'
+          handle_etag element, xml
         when 'Char'
-          xml << get_char(element)
+          last_line = xml.pop
+          last_line += get_char(element)
+          add last_line
+        when 'Para'
+          handle_para xml
         when 'PgfNumString'
-          string = clean(element)
-          if string
-            xml << "<PgfNumString>#{string}</PgfNumString>"
+          if @pgf_tag
+            add "\n<#{@pgf_tag}>"
+            @in_paragraph = true
           end
-        when 'String'
           string = clean(element)
-          xml << string
+          add "<PgfNumString>#{string}</PgfNumString>"
+        when 'String'
+          last_line = xml.pop
+          last_line += clean(element)
+          add last_line
         when 'ElementEnd'
-          tag = stack.pop
-          xml << '</'
-          xml << tag
-          xml << '>'
-          xml << "\n" unless tag[/(Day|STHouse|STLords|STText)/]
+          handle_element_end element, xml
       end
     end
   end
--- spec/lib/mifparser_spec.rb
+++ spec/lib/mifparser_spec.rb
@@ -36,7 +36,7 @@ describe MifParser do
       @result = @parser.parse_xml(fixture('pbc0900206m.mif.xml'))
     end
     it 'should make PgfTag into paragraphs' do
-      # puts @result
+      puts @result
     end
   end
 
@@ -53,30 +53,63 @@ describe MifParser do
     end
 
     it 'should make ETags into elements' do
-      puts @result
-      @result.should == "<Document><Amendments.Commons><Head><HeadNotice><NoticeOfAmds>Notices of Amendments</NoticeOfAmds>
-<Given>given on</Given>
-<Date><Day>Monday </Day><Date.text>1 June 2009</Date.text>
+      @result.should == "<Document><Amendments.Commons id='1020493'><Head id='1033398'><HeadNotice id='1033750'><NoticeOfAmds id='1042951'>Notices of Amendments</NoticeOfAmds>
+<Given id='1045577'>given on</Given>
+<Date id='1041467'><Day id='1041470'>Monday </Day><Date.text id='1041477'>1 June 2009</Date.text>
 </Date>
-<Stageheader>Public Bill Committee</Stageheader>
-<CommitteeShorttitle><STText>Local Democracy, Economic Development and Construction Bill</STText><STHouse> [<STLords>Lords</STLords>]</STHouse></CommitteeShorttitle>
+<Stageheader id='1045600'>Public Bill Committee</Stageheader>
+<CommitteeShorttitle id='1045605'><STText id='1053525'>Local Democracy, Economic Development and Construction Bill</STText><STHouse id='5229516'> [<STLords id='5229520'>Lords</STLords>]</STHouse></CommitteeShorttitle>
 </HeadNotice>
 </Head>
-<PgfNumString>\\t</PgfNumString><Committee><NewClause.Committee><ClauseTitle>Award of construction projects</ClauseTitle>
-<Sponsors><Sponsor>Ms Sally Keeble</Sponsor>
-<Sponsor>Ms Dari Taylor</Sponsor>
-<Sponsor>Joan Walley</Sponsor>
+<Committee id='1150928'><NewClause.Committee id='1497582'>
+<New_C_STitle><PgfNumString>\\t</PgfNumString><ClauseTitle id='1497590'>Award of construction projects</ClauseTitle></New_C_STitle>
+<Sponsors id='1497599'><Sponsor id='1497603'>Ms Sally Keeble</Sponsor>
+<Sponsor id='1497609'>Ms Dari Taylor</Sponsor>
+<Sponsor id='1497618'>Joan Walley</Sponsor>
 </Sponsors>
-<Amendment.Number>NC1</Amendment.Number>
-<Move>To move the following Clause:—</Move>
-<ClauseText>‘In considering the award of a contract in accordance with the Housing Grant, Construction and Regeneration Act 1996 (c. 53), a local authority may have regard to—<PgfNumString>\\t(a)\\t</PgfNumString><Para>their functions under section 66 of the Local Democracy, Economic Development and Construction Act 2009 (local authority economic assessment); and</Para>
-<PgfNumString>\\t(b)\\t</PgfNumString><Para>the desirability of maintaining a diverse range of contractors in its local authority area.’.</Para>
+<Amendment.Number id='1497632'>NC1</Amendment.Number>
+<Move id='1497648'>To move the following Clause:—</Move>
+<ClauseText id='1497659'>‘In considering the award of a contract in accordance with the Housing Grant, Construction and Regeneration Act 1996 (c. 53), a local authority may have regard to—
+<Paragraph><PgfNumString>\\t(a)\\t</PgfNumString><Para id='1497669'>their functions under section 66 of the Local Democracy, Economic Development and Construction Act 2009 (local authority economic assessment); and</Para></Paragraph>
+
+<Paragraph><PgfNumString>\\t(b)\\t</PgfNumString><Para id='1497676'>the desirability of maintaining a diverse range of contractors in its local authority area.’.</Para></Paragraph>
 </ClauseText>
 </NewClause.Committee>
  </Committee>
 </Amendments.Commons>
 </Document>"
-      # @result.gsub('.','-').should have_tag('Amendments-Commons') do
+
+"<Document><ClauseText id='1020493'><ClauseText id='1033398'><ClauseText id='1033750'><ClauseText id='1042951'>Notices of Amendments</NoticeOfAmds>
+<ClauseText id='1045577'>given on</Given>
+<Date id='1041467'><ClauseText id='1041470'>Monday </Day><ClauseText id='1041477'>1 June 2009</Date.text>
+</Date>
+<ClauseText id='1045600'>Public Bill Committee</Stageheader>
+<CommitteeShorttitle id='1045605'><ClauseText id='1053525'>Local Democracy, Economic Development and Construction Bill</STText><ClauseText id='5229516'> [<ClauseText id='5229520'>Lords</STLords>]</STHouse></CommitteeShorttitle>
+</HeadNotice>
+</Head>
+<ClauseText id='1150928'><NewClause.Committee id='1497582'><ClauseText id='1497590'>
+<New_C_STitle><PgfNumString>\\t</PgfNumString>Award of construction projects</New_C_STitle>
+</ClauseTitle><Sponsors id='1497599'><ClauseText id='1497603'>Ms Sally Keeble</Sponsor>
+<ClauseText id='1497609'>Ms Dari Taylor</Sponsor>
+<ClauseText id='1497618'>Joan Walley</Sponsor>
+</Sponsors>
+<ClauseText id='1497632'>NC1</Amendment.Number>
+<ClauseText id='1497648'>To move the following Clause:—</Move>
+<ClauseText id='1497659'>‘In considering the award of a contract in accordance with the Housing Grant, Construction and Regeneration Act 1996 (c. 53), a local authority may have regard to—<ClauseText id='1497669'>
+<Paragraph><PgfNumString>\\t(a)\\t</PgfNumString>their functions under section 66 of the Local Democracy, Economic Development and Construction Act 2009 (local authority economic assessment); and</Paragraph>
+</Para><ClauseText id='1497676'>
+<Paragraph><PgfNumString>\\t(b)\\t</PgfNumString>the desirability of maintaining a diverse range of contractors in its local authority area.’.</Paragraph>
+</Para></ClauseText>
+</NewClause.Committee>
+ </Committee>
+</Amendments.Commons>
+</Document>"
+
+
+
+
+
+# @result.gsub('.','-').should have_tag('Amendments-Commons') do
         # with_tag('Head') do
           # with_tag('HeadNotice') do
             # with_tag('NoticeOfAmds', :text => 'Notices of Amendments')
