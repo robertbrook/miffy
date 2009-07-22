6889664dd7257c267295fdbf10ca93eddfe023f6
lizconlan <Liz@liz-conlans-macbook.local>
2009-07-22 10:37:38 +0100

    improved file_type / stylesheet handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def file_type file_name
+    if file_name.include?('Clauses.mif')
+      "clauses"
+    elsif (file_name =~ /pbc(.)*m.mif/).is_a?(Fixnum)
+      "amendment_paper"
+    elsif (file_name =~ /pbc(.)*a.mif/).is_a?(Fixnum)
+      "marshalled_list"
+    else
+      "coverted"
+    end
+  end
+  
 end
