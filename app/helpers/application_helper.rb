d53af3b615ff719c45ed8a2546661e8482f15dd9
lizconlan <Liz@liz-conlans-macbook.local>
2009-07-22 11:57:38 +0100

    added more file_types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,16 @@ module ApplicationHelper
   def file_type file_name
     if file_name.include?('Clauses.mif')
       "clauses"
+    elsif file_name.include?('Cover.mif')
+      "cover"
+    elsif file_name.include?('Arrangement.mif')
+      "arrangement"
     elsif (file_name =~ /pbc(.)*m.mif/).is_a?(Fixnum)
       "amendment_paper"
     elsif (file_name =~ /pbc(.)*a.mif/).is_a?(Fixnum)
       "marshalled_list"
     else
-      "coverted"
+      "converted"
     end
   end
   
