582c5dfeff45fc21a94670ddc9bde77349645234
robmckinnon <rob@theyworkforyou.co.nz>
2009-09-10 10:52:17 +0100

    put longer form of the explanatory note Clause title text in a span when html formatting

--- app/models/explanatory_note.rb
+++ app/models/explanatory_note.rb
@@ -11,12 +11,14 @@ class ExplanatoryNote < ActiveRecord::Base
     html = tokens.collect do |token|
       lines = token.split("\n")
       result = []
-      if lines.first[/^\s*Clause (\d+)\s*$/]
+
+      if lines.first[/^\s*Clause (\d+|\d+:.+)\s*$/]
         result << '<span class="NoteClauseTitle">Clause ' + $1 + '</span>'
         make_paragraph result, lines, adj=1
       else
         make_paragraph result, lines
       end
+
       result.join(' ')
     end
 
@@ -26,15 +28,17 @@ class ExplanatoryNote < ActiveRecord::Base
   private
 
     def make_paragraph html, lines, adj=0
+      first_line = lines[0+adj].strip
+
       if (lines.size-adj) == 1
-        html << "<p>#{lines[0+adj]}</p>"
+        html << "<p>#{first_line}</p>"
       else
-        html << "<p>#{lines[0+adj]}"
+        html << "<p>#{first_line}"
         if lines.size > 2+adj
           rest = lines[(1+adj)..(lines.size-2)]
-          rest.each {|line| html << line}
+          rest.each {|line| html << line.strip}
         end
-        html << "#{lines.last}</p>"
+        html << "#{lines.last.strip}</p>"
       end
     end
 
--- spec/models/explanatory_note_spec.rb
+++ spec/models/explanatory_note_spec.rb
@@ -2,30 +2,53 @@ require File.dirname(__FILE__) + '/../spec_helper.rb'
 
 describe ExplanatoryNote do
 
-  describe 'when create html note text' do
-    before(:all) do
-      @note = ExplanatoryNote.new :note_text => '  Clause 1 ' + '
+  describe 'when asked for html note text' do
+
+    describe 'and clause title includes title name' do
+      before(:all) do
+        @note = ExplanatoryNote.new :note_text => '  Clause 3: Royal Mail Companies to be publicly owned
+        27.     Subsection (1) requires that each Royal Mail company must be publicly owned. To ensure that
+        this is the case, subsection (2)(a) and (b) provide that any issue or transfer of shares or of share
+        rights in a company is ineffective if it would cause a Royal Mail company to cease to be publicly owned.
+        The meaning of Royal Mail company is set out in clause 4.
+        Clauses 10 to 12 contain further details as to what is meant by “publicly owned”. '
+        @html = @note.html_note_text
+      end
+
+      it 'should wrap Clause heading in clause_title span' do
+        @html.should include('<span class="NoteClauseTitle">Clause 3: Royal Mail Companies to be publicly owned</span>')
+      end
+
+      it 'should wrap paragraph in p element' do
+        @html.should include('<p>27.     Subsection (1) requires that each Royal Mail company must be publicly owned. To ensure that this is the case, subsection (2)(a) and (b) provide that any issue or transfer of shares or of share rights in a company is ineffective if it would cause a Royal Mail company to cease to be publicly owned. The meaning of Royal Mail company is set out in clause 4. Clauses 10 to 12 contain further details as to what is meant by “publicly owned”.</p>')
+      end
+    end
+
+    describe 'and clause title does not include title name' do
+      before(:all) do
+        @note = ExplanatoryNote.new :note_text => '  Clause 1 ' + '
 7. Under section 6(1) of the Railways Act 2005 (“the 2005 Act”) the Secretary of State
 has the power to “provide, or agree to provide, financial assistance to any person –
  ' + '
-(a) for the purpose of securing the provision, improvement or development of railway
+  (a) for the purpose of securing the provision, improvement or development of railway
 services or railway assets; or
 
 (b) for any other purpose relating to a railway or to railway services.”
 '
-      @html = @note.html_note_text
-    end
+        @html = @note.html_note_text
+      end
 
-    it 'should wrap Clause heading in clause_title span' do
-      @html.should include('<span class="NoteClauseTitle">Clause 1</span>')
-    end
+      it 'should wrap Clause heading in clause_title span' do
+        @html.should include('<span class="NoteClauseTitle">Clause 1</span>')
+      end
 
-    it 'should paragraphs in p elements' do
-      @html.should include('<p>(a) for the purpose of securing the provision, improvement or development of railway services or railway assets; or</p>')
+      it 'should wrap paragraphs in p elements' do
+        @html.should include('<p>(a) for the purpose of securing the provision, improvement or development of railway services or railway assets; or</p>')
 
-      @html.should include('<p>(b) for any other purpose relating to a railway or to railway services.”</p>')
+        @html.should include('<p>(b) for any other purpose relating to a railway or to railway services.”</p>')
 
-      @html.should include('<p>7. Under section 6(1) of the Railways Act 2005 (“the 2005 Act”) the Secretary of State has the power to “provide, or agree to provide, financial assistance to any person –</p>')
+        @html.should include('<p>7. Under section 6(1) of the Railways Act 2005 (“the 2005 Act”) the Secretary of State has the power to “provide, or agree to provide, financial assistance to any person –</p>')
+      end
     end
   end
 
