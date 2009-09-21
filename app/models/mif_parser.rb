8df84bfec3d7bca7fa739d4657f838af98fdda79
lizconlan <Liz@liz-conlans-macbook.local>
2009-09-21 17:44:09 +0100

    MifParser adding FileType element to the mif xml

--- app/models/mif_parser.rb
+++ app/models/mif_parser.rb
@@ -110,10 +110,39 @@ class MifParser
     @citations = []
   end
 
+  def get_file_type doc
+    file_type = ""
+    
+    stage_para = doc.search("//TextFlow[Para]").search("Para[text()*=`StageHeader']")
+    if stage_para.size == 2
+      if clean(stage_para[0]/'String/text()').downcase.strip == "notices of amendments"
+        file_type = "Marshalled List"
+      end
+    else
+      if clean(stage_para/'String/text()') && clean(stage_para/'String/text()').downcase.strip == "consideration of bill"
+        header_para = doc.search("//TextFlow[Para]").search("Para[text()*=`Header']/ParaLine")
+        if clean(header_para.last/'String/text()').downcase.strip =~ /tabled/
+          file_type = "Tabled "
+        end
+        file_type += "Report"
+      elsif clean(stage_para/'String/text()') && clean(stage_para/'String/text()').downcase.strip =~ /committee$/
+        file_type = "Amendments"
+      else
+        etag_elements = (doc/'TextFlow/Para/ParaLine/ElementBegin/ETag/text()').to_s.gsub("`","").split("'")
+        if etag_elements.include?("Clauses") && etag_elements.include?("WordsOfEnactment")
+          file_type = "Clauses"
+        end 
+      end
+    end
+    file_type = "Other" if file_type == ""
+    file_type
+  end
+  
   def make_xml doc
     initialize_doc_state doc
     @xml = ['<Document><BillData>']
     add_bill_attribute 'ShortTitle', 'BillTitle'
+    add "<FileType>#{get_file_type(doc)}</FileType>"
     (doc/'TextFlow').each do |flow|
       handle_flow(flow) unless is_instructions?(flow)
     end
