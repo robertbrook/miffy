689b023c4413adf10f14a41aa11cd559b69c5d2b
robmckinnon <rob@theyworkforyou.co.nz>
2009-09-22 15:14:57 +0100

    make reference to an act section a hyperlink

--- spec/models/act_reference_parser_spec.rb
+++ spec/models/act_reference_parser_spec.rb
@@ -14,10 +14,18 @@ describe ActReferenceParser do
       File.open(RAILS_ROOT + '/spec/fixtures/ChannelTunnel/ChannelTunnelClauses.act.xml','w') {|f| f.write @result }
     end
 
-    it 'should put rel cite anchor element around abbrebrivated act name' do
+    # it 'should put rel cite anchor element around abbrebrivated act name' do
+      # @result.should have_tag('ClauseText[id="1113674"]') do
+        # with_tag('a[rel="cite"]', :text => 'the 1996 Act')
+        # with_tag('a[resource="http://www.legislation.gov.uk/ukpga/1996/61"]')
+        # with_tag('a[href="http://www.opsi.gov.uk/acts/acts1996/ukpga_19960061_en_1"]')
+      # end
+    # end
+
+    it 'should put rel cite anchor element around reference to section of act' do
       @result.should have_tag('ClauseText[id="1113674"]') do
-        with_tag('a[rel="cite"]', :text => 'the 1996 Act')
-        with_tag('a[resource="http://www.legislation.gov.uk/ukpga/1996/61"]')
+        with_tag('a[rel="cite"]', :text => 'section 56 of the 1996 Act')
+        with_tag('a[resource="http://www.legislation.gov.uk/ukpga/1996/61/56"]')
         with_tag('a[href="http://www.opsi.gov.uk/acts/acts1996/ukpga_19960061_en_1"]')
       end
     end
