5b78e14970d01dfa9c857d82fe47782fe2960f10
robmckinnon <rob@theyworkforyou.co.nz>
2009-10-27 12:37:54 +0000

    table parser now produces well-formed xml when table ends in table header row

--- app/models/mif_table_parser.rb
+++ app/models/mif_table_parser.rb
@@ -15,68 +15,105 @@ class MifTableParser
     end
   end
 
-  def handle_table table_xml, tables
-    hash_size = tables.size
-    @current_table_id = nil
-    @in_row = false
-    @in_cell= false
-    @in_heading = false
+  def handle_id node
+    @current_table_id = node.at('text()').to_s
+  end
 
-    table_xml.traverse_element do |element|
-      case element.name
-        when 'TblID'
-          @current_table_id = element.at('text()').to_s
-        when 'TblTag'
-          tag = clean(element)
-          if tag != 'Table' && tag != 'RepealContinue'
-            break
-          else
-            tables.merge!({@current_table_id, start_tag('TableData', element)})
-          end
-        when 'Row'
-          if @in_row
-            tables[@current_table_id] << "</Cell></Row>"
-            @in_row = false
-            @in_cell = false
-          end
-          row_id = element.at('Element/Unique/text()').to_s
-          @in_row = true
-          tables[@current_table_id] << %Q|<Row id="#{row_id}">|
-        when 'Cell'
-          first = ' class="first" '
-          if @in_cell
-            first = ""
-            if @in_heading
-              tables[@current_table_id] << "</CellH>"
-            else
-              tables[@current_table_id] << "</Cell>"
-            end
-          end
-          cell_id = element.at('Element/Unique/text()').to_s
-          @in_cell = true
-          if @in_heading
-            tables[@current_table_id] << %Q|<CellH id="#{cell_id}"#{first}>|
-          else
-            tables[@current_table_id] << %Q|<Cell id="#{cell_id}"#{first}>|
-          end
-        when 'TblH'
-          @in_heading = true
-        when 'TblBody'
-          tables[@current_table_id] << '</CellH></Row>' if @in_heading
-          @in_row = false
-          @in_cell = false
-          @in_heading = false
-        when 'String'
-          text = clean(element)
-          tables[@current_table_id] << text
-        when 'Char'
-          text = get_char(element)
-          tables[@current_table_id] << text
+  def handle_tag node, tables
+    tag = clean(node)
+    if tag != 'Table' && tag != 'RepealContinue'
+      do_break = true
+    else
+      tables.merge!({@current_table_id, start_tag('TableData', node)})
+      do_break = false
+    end
+    do_break
+  end
+
+  def handle_row node, tables
+    if @in_row
+      tables[@current_table_id] << "</Cell></Row>"
+      @in_row = false
+      @in_cell = false
+    end
+    row_id = node.at('Element/Unique/text()').to_s
+    @in_row = true
+    tables[@current_table_id] << %Q|<Row id="#{row_id}">|
+  end
+
+  def handle_cell node, tables
+    first = ' class="first" '
+    if @in_cell
+      first = ""
+      if @in_heading
+        tables[@current_table_id] << "</CellH>"
+      else
+        tables[@current_table_id] << "</Cell>"
       end
     end
+    cell_id = node.at('Element/Unique/text()').to_s
+    @in_cell = true
+    if @in_heading
+      tables[@current_table_id] << %Q|<CellH id="#{cell_id}"#{first}>|
+    else
+      tables[@current_table_id] << %Q|<Cell id="#{cell_id}"#{first}>|
+    end
+  end
 
-    if tables.size > hash_size
-      tables[@current_table_id] << "</Cell></Row></TableData>"
+  def handle_body tables
+    tables[@current_table_id] << '</CellH></Row>' if @in_heading
+    @in_row = false
+    @in_cell = false
+    @in_heading = false
+  end
+
+  def add_text text, tables
+    tables[@current_table_id] << text
+  end
+
+  def handle_node node, tables
+    do_break = false
+
+    case node.name
+      when 'TblID'
+        handle_id node
+      when 'TblTag'
+        do_break = handle_tag(node, tables)
+      when 'Row'
+        handle_row node, tables
+      when 'Cell'
+        handle_cell node, tables
+      when 'TblH'
+        @in_heading = true
+      when 'TblBody'
+        handle_body tables
+      when 'String'
+        add_text clean(node), tables
+      when 'Char'
+        add_text get_char(node), tables
+    end
+
+    do_break
+  end
+
+  def handle_table table_xml, tables
+    @current_table_id = nil
+    @in_heading = false
+    @in_row = false
+    @in_cell= false
+    table_count = tables.size
+
+    table_xml.traverse_element do |node|
+      do_break = handle_node node, tables
+      break if do_break
+    end
+
+    if tables.size > table_count
+      if @in_heading
+        add_text "</CellH></Row></TableData>", tables
+      else
+        add_text "</Cell></Row></TableData>", tables
+      end
     end
 
     tables
---
+++ spec/models/mif_table_parser_spec.rb
@@ -0,0 +1,172 @@
+require File.dirname(__FILE__) + '/../spec_helper.rb'
+
+require 'action_controller'
+require 'action_controller/assertions/selector_assertions'
+include ActionController::Assertions::SelectorAssertions
+
+
+describe MifTableParser do
+
+  it 'should create table' do
+    doc = Hpricot.XML TABLE
+    tables = MifTableParser.new.get_tables doc
+    tables.to_a.first[1].to_s.should == '<TableData id="1147573">
+<Row id="1146883">
+<CellH id="1146884" class="first" >CO2 emissions figure</CellH>
+<CellH id="1146885"></CellH>
+<CellH id="1146886">Rate</CellH>
+<CellH id="1146887"></CellH>
+</Row></TableData>'.gsub("\n",'')
+  end
+
+  TABLE = "
+<Tbls><Tbl>
+  <TblID>21</TblID>
+  <TblTag>`Table'</TblTag>
+  <TblNumColumns>4</TblNumColumns>
+  <Unique>1147573</Unique>
+  <TblColumnWidth>6.90551 pc</TblColumnWidth>
+  <TblColumnWidth>6.84301 pc</TblColumnWidth>
+  <TblColumnWidth>6.90551 pc</TblColumnWidth>
+  <TblColumnWidth>6.90551 pc</TblColumnWidth>
+  <Element>
+    <Unique>1146880</Unique>
+    <ETag>`Tgroup'</ETag>
+    <Attributes></Attributes>
+    <Collapsed>No</Collapsed>
+    <SpecialCase>No</SpecialCase>
+    <AttributeDisplay>None</AttributeDisplay>
+  </Element>
+  <TblH>
+    <Element>
+      <Unique>1146881</Unique>
+      <ETag>`Thead'</ETag>
+      <Attributes></Attributes>
+      <Collapsed>No</Collapsed>
+      <SpecialCase>No</SpecialCase>
+      <AttributeDisplay>None</AttributeDisplay>
+    </Element>
+    <Row>
+      <RowMaxHeight>84.0 pc</RowMaxHeight>
+      <RowHeight>2.08333 pc</RowHeight>
+      <Element>
+        <Unique>1146883</Unique>
+        <ETag>`Row'</ETag>
+        <Attributes></Attributes>
+        <Collapsed>Yes</Collapsed>
+        <SpecialCase>No</SpecialCase>
+        <AttributeDisplay>None</AttributeDisplay>
+      </Element>
+      <Cell>
+        <CellColumns>2</CellColumns>
+        <CellLRuling>`Thin'</CellLRuling>
+        <CellBRuling>`Thin'</CellBRuling>
+        <CellTRuling>`Thin'</CellTRuling>
+        <Element>
+          <Unique>1146884</Unique>
+          <ETag>`Entry'</ETag>
+          <Attributes></Attributes>
+          <Collapsed>No</Collapsed>
+          <SpecialCase>No</SpecialCase>
+          <AttributeDisplay>None</AttributeDisplay>
+        </Element>
+        <CellContent>
+          <Notes></Notes>
+          <Para>
+            <Unique>1147578</Unique>
+            <PgfTag>`CellHeading'</PgfTag>
+            <ParaLine>
+              <String>`CO'</String>
+              <ElementBegin>
+                <Unique>1147211</Unique>
+                <ETag>`Sbscript'</ETag>
+                <Attributes></Attributes>
+                <Collapsed>No</Collapsed>
+                <SpecialCase>No</SpecialCase>
+                <AttributeDisplay>None</AttributeDisplay>
+              </ElementBegin>
+              <Font>
+                <FTag>`Subscript'</FTag>
+                <FLocked>No</FLocked>
+              </Font>
+              <String>`2'</String>
+              <Font>
+                <FTag>`'</FTag>
+                <FLocked>No</FLocked>
+              </Font>
+              <ElementEnd>`Sbscript'</ElementEnd>
+              <String>` emissions figure'</String>
+            </ParaLine>
+          </Para>
+        </CellContent>
+      </Cell>
+      <Cell>
+        <CellBRuling>`Thin'</CellBRuling>
+        <CellTRuling>`Thin'</CellTRuling>
+        <Element>
+          <Unique>1146885</Unique>
+          <ETag>`Entry'</ETag>
+          <Attributes></Attributes>
+          <Collapsed>No</Collapsed>
+          <SpecialCase>No</SpecialCase>
+          <AttributeDisplay>None</AttributeDisplay>
+        </Element>
+        <CellContent>
+          <Notes></Notes>
+          <Para>
+            <Unique>1147580</Unique>
+            <PgfTag>`CellHeading'</PgfTag>
+            <ParaLine></ParaLine>
+          </Para>
+        </CellContent>
+      </Cell>
+      <Cell>
+        <CellColumns>2</CellColumns>
+        <CellLRuling>`Thin'</CellLRuling>
+        <CellBRuling>`Thin'</CellBRuling>
+        <CellRRuling>`Thin'</CellRRuling>
+        <CellTRuling>`Thin'</CellTRuling>
+        <Element>
+          <Unique>1146886</Unique>
+          <ETag>`Entry'</ETag>
+          <Attributes></Attributes>
+          <Collapsed>No</Collapsed>
+          <SpecialCase>No</SpecialCase>
+          <AttributeDisplay>None</AttributeDisplay>
+        </Element>
+        <CellContent>
+          <Notes></Notes>
+          <Para>
+            <Unique>1147582</Unique>
+            <PgfTag>`CellHeading'</PgfTag>
+            <ParaLine>
+              <String>`Rate'</String>
+            </ParaLine>
+          </Para>
+        </CellContent>
+      </Cell>
+      <Cell>
+        <CellBRuling>`Thin'</CellBRuling>
+        <CellRRuling>`Thin'</CellRRuling>
+        <CellTRuling>`Thin'</CellTRuling>
+        <Element>
+          <Unique>1146887</Unique>
+          <ETag>`Entry'</ETag>
+          <Attributes></Attributes>
+          <Collapsed>No</Collapsed>
+          <SpecialCase>No</SpecialCase>
+          <AttributeDisplay>None</AttributeDisplay>
+        </Element>
+        <CellContent>
+          <Notes></Notes>
+          <Para>
+            <Unique>1147584</Unique>
+            <PgfTag>`CellHeading'</PgfTag>
+            <ParaLine></ParaLine>
+          </Para>
+        </CellContent>
+      </Cell>
+    </Row>
+  </TblH>
+</Tbl></Tbls>"
+end
